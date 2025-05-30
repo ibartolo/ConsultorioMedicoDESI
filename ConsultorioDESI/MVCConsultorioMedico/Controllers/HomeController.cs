31da08c3e37058aa14e7a3d9c3625f533d3c84ef
Ivan <bartolocastro@gmail.com>
2025-05-29 21:59:56 -0600

    Se sube el proyecto al repositorio

--- ConsultorioDESI/MVCConsultorioMedico/Controllers/HomeController.cs
+++ ConsultorioDESI/MVCConsultorioMedico/Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace MVCConsultorioMedico.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
         public ActionResult Index()
         {
             return View();
