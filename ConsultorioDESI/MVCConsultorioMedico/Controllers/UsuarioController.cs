a93d8a710829c45d2283601e2229a168b5ac465b
Victor Manuel <vmhl02012003@gmail.com>
2025-06-17 21:01:39 -0600

    Ingresando controladores, httpclientconnection , objetos y vistas

---
+++ ConsultorioDESI/MVCConsultorioMedico/Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using MVCConsultorioMedico.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MVCConsultorioMedico.DAL;
+
+namespace MVCConsultorioMedico.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public async Task<ActionResult> Index()
+        {
+            //especificar el metodo que voy a utilizar para obtener usuario por id
+
+            ObjUsuario obj = new ObjUsuario();
+            obj = await new HttpClientConnection().GetUsuarioById(2);
+            return View(obj);
+        }
+    }
+}
---
+++ ConsultorioDESI/MVCConsultorioMedico/Models/ObjUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MVCConsultorioMedico.Models
+{
+    public class ObjUsuario
+    {
+        public string UserName { get; set; }
+        public string Pass { get; set; }
+        public string Email { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
+    
