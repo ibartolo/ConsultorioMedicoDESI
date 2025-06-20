c9090d8de83af4117233bd552a8e43729c4f1003
Ivan <bartolocastro@gmail.com>
2025-06-19 21:03:50 -0600

    e agrega validador para la ventana de empresas

---
+++ ConsultorioDESI/MVCConsultorioMedico/Scripts/Empresa/Index.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    ValidarDatos();
+});
+
+function ValidarDatos() {
+    $("#frmEmpresa").validate({
+        rules: {
+            "Nombre": {
+                required: true,
+                maxlength: 15,
+                minlength: 4
+            },
+            "EmailContacto": {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            "Nombre": {
+                required: "El campo Nombre es requerido.",
+                maxlength: "El nombre no puede ser mayor a 15 caracteres.",
+                minlength: "El nombre tiene que ser mayor a 4 caracteres."
+            }
+        }
+    });
+}
