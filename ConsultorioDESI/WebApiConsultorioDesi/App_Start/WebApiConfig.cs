d65d827962e5dc0c9dade98bec4a2ba61d252187
Ivan <bartolocastro@gmail.com>
2025-07-30 23:38:09 -0600

    Se realizan cambios para poder solicitar el token en la web api

--- ConsultorioDESI/WebApiConsultorioDesi/App_Start/WebApiConfig.cs
+++ ConsultorioDESI/WebApiConsultorioDesi/App_Start/WebApiConfig.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace WebApiConsultorioDesi
@@ -9,9 +11,8 @@ namespace WebApiConsultorioDesi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Configuración y servicios de Web API
 
-            // Rutas de Web API
+            // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
@@ -19,6 +20,9 @@ namespace WebApiConsultorioDesi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
